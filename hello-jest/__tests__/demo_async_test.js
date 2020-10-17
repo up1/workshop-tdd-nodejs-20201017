2d9459464c92e98c2049fcfc96a5a8d65b8ff691
up1 <somkiat.p@gmail.com>
2020-10-17 15:30:28 +0700

    Demo with async

---
+++ hello-jest/__tests__/demo_async_test.js
@@ -0,0 +1,19 @@
+const { addAsyncCallback } = require("../src/demo_async");
+const cal = require("../src/demo_async");
+
+test("add numbers async", (done) => {
+  cal.addAsyncCallback(5, 10, (result) => {
+    expect(result).toBe(15);
+    done();
+  });
+});
+
+test("should resolve to some value", () => {
+  const p = cal.addAsyncPromise(10, 5);
+  return expect(p).resolves.toBe(15);
+});
+
+test("shows how async / await works", async () => {
+  const value = await cal.addAsyncPromise(10, 5);
+  expect(value).toBe(15);
+});
