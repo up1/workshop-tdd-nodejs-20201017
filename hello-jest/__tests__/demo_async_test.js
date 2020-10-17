fdd10f1f3248a491dc6e6061393799dbdb8ea820
up1 <somkiat.p@gmail.com>
2020-10-17 16:11:21 +0700

    Working with reject in Promise

--- hello-jest/__tests__/demo_async_test.js
+++ hello-jest/__tests__/demo_async_test.js
@@ -1,4 +1,3 @@
-const { addAsyncCallback } = require("../src/demo_async");
 const cal = require("../src/demo_async");
 
 test("add numbers async", (done) => {
@@ -16,4 +15,8 @@ test("should resolve to some value", () => {
 test("shows how async / await works", async () => {
   const value = await cal.addAsyncPromise(10, 5);
   expect(value).toBe(15);
+});
+
+test("shows how async / await works with reject", async () => {
+  await cal.addAsyncPromise(5, 10).catch((value) => expect(value).toBe(15));
 });
