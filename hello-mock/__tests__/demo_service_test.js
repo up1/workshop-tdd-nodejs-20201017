1feb47231f9ff274db8682204d69e3624a6c9fee
up1 <somkiat.p@gmail.com>
2020-10-18 11:00:37 +0700

    Working with jest mock

--- hello-mock/__tests__/demo_service_test.js
+++ hello-mock/__tests__/demo_service_test.js
@@ -1,6 +1,25 @@
 const demoService = require("../src/demo_service");
 
+beforeEach(() => {
+  jest.spyOn(global.Math, "random").mockReturnValue(0.5);
+});
+
+afterEach(() => {
+  jest.spyOn(global.Math, "random").mockRestore();
+});
+
 test("should get number 5 from demo service", () => {
   const result = demoService.getNumber(null);
   expect(result).toBe(5);
+});
+
+test("should be called Math.random", () => {
+  const spy = jest.spyOn(global.Math, "random");
+  demoService.getNumber(null);
+  expect(spy).toBeCalled();
+  expect(spy).toBeCalledTimes(1);
+});
+
+afterAll(() => {
+  jest.clearAllMocks();
 });
