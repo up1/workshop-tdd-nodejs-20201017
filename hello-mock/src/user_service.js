24695583396723c5b3c61a915f84e2ef8dc9beec
up1 <somkiat.p@gmail.com>
2020-10-18 08:11:26 +0700

    Add repository layer to connect to database

--- hello-mock/src/user_service.js
+++ hello-mock/src/user_service.js
@@ -1,5 +1,4 @@
-const getAllUser = require("./user_gateway.js");
-const userGateway = require("./user_gateway.js");
+const userGateway = require("./user_gateway");
 
 const searchUser = async (user) => {
   return await userGateway.getAllUser();
