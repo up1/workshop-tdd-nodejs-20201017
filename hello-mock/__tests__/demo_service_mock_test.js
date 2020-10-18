c9720e3989b65ac2d671e453795bf6f66725dde3
up1 <somkiat.p@gmail.com>
2020-10-18 09:04:14 +0700

    Demo with mock

---
+++ hello-mock/__tests__/demo_service_mock_test.js
@@ -0,0 +1,17 @@
+const demoService = require("../src/demo_service");
+
+beforeEach(() => {
+  // jest.spyOn(global.Math, "random").mockReturnValue(0.5);
+  const mockMath = Object.create(global.Math);
+  mockMath.random = () => 0.5;
+  global.Math = mockMath;
+});
+
+afterEach(() => {
+  // jest.spyOn(global.Math, "random").mockRestore();
+});
+
+test("should get number 5 from demo service", () => {
+  const result = demoService.getNumber(null);
+  expect(result).toBe(5);
+});
